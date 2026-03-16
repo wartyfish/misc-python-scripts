90feef6e5fa8301f401f85491c9553c23af4e4f7
wartyfish <jamiefendt96@gmail.com>
2026-03-16 00:04:01 +0000

    minor updates to tabler

---
+++ Tabler/tabler.bat
@@ -0,0 +1,4 @@
+@call %HOMEDRIVE%%HOMEPATH%\Scripts\.venv\Scripts\activate.bat
+@python %HOMEDRIVE%%HOMEPATH%\Scripts\tabler.py %*
+@pause 		
+@deactivate
--- Tabler/tabler.py
+++ Tabler/tabler.py
@@ -1,41 +1,47 @@
 import pyperclip
 
-copied = pyperclip.paste()
+def main():
+    copied = pyperclip.paste()
 
-rows = []
-row = []
+    rows = []
+    row = []
 
-for data in copied.split("\t"):    
-    cell = data.split("\r\n")
+    for data in copied.split("\t"):    
+        cell = data.split("\r\n")
 
-    row.append(cell.pop(0))
-        
-    while len(cell) > 0:
-        rows.append(row)
-        row = [cell.pop(0)]
+        row.append(cell.pop(0))
+            
+        while cell:
+            rows.append(row)
+            row = [cell.pop(0)]
 
-rows.append(row)
+    rows.append(row)
 
-header = "|"
-header += "|".join(cell for cell in rows[0])
-header += "|\n"
-
-
-divider = "|"
-
-for col_index in range(len(rows[0])):
-    divider += " --- |"
-
-divider += "\n"
+    header = "|"
+    header += "|".join(rows[0])
+    header += "|\n"
 
 
-body = "|"
+    divider = "|"
 
-for row in rows[1:]:
-    body += "|"
-    body += "|".join(cell for cell in row)
-    body += "|\n"
+    for col_index in range(len(rows[0])):
+        divider += " --- |"
 
-table = header + divider + body
+    divider += "\n"
 
-pyperclip.copy(table)
+
+    for row in rows[1:]:
+        body += "|"
+        body += "|".join(cell for cell in row)
+        body += "|\n"
+
+    table = header + divider + body
+
+    pyperclip.copy(table)
+
+
+try:
+    main()
+    print("Table copied to clipboard.")
+except :
+    print("Failed to copy table to clipboard.")
